f12c3ce9c0992d4c983b00090ab1dba5a12ccd3c
finu fayad <caterly95@gmail.com>
2026-08-02 12:41:27 +0530

    Update

--- push.bat
+++ push.bat
@@ -1,6 +1,8 @@
 @echo off
+set /p msg="Enter commit message: "
+if "%msg%"=="" set msg=Update
 git add .
-git commit -m "Update Dare UI to show one card at a time"
+git commit -m "%msg%"
 git push -u origin main
 echo Push complete!
 pause
